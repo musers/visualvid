7f2cad68dc1a0b4cd80f832db8c97e1052831a30
Srikanth Boddupally <sboddupally@evoketechnologies.com>
2020-09-04 19:18:35 +0530

    Adding how-it-works component in HomePage

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -5,9 +5,10 @@ import { VisualvidSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 import { VideoDesignsModule } from 'app/videodesigns/videodesigns.module';
+import { HowItWorksComponent } from 'app/how-it-works/how-it-works.component';
 
 @NgModule({
   imports: [VisualvidSharedModule, VideoDesignsModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HowItWorksComponent],
 })
 export class VisualvidHomeModule {}
