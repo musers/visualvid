3b5e405e259402b8aec7a0fc7043d9e46e7acbf1
Bala <>
2020-09-20 22:04:38 +0530

    Created missing s3 bean

--- src/main/java/com/ae/visuavid/config/ApplicationConfiguration.java
+++ src/main/java/com/ae/visuavid/config/ApplicationConfiguration.java
@@ -2,10 +2,6 @@ package com.ae.visuavid.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
@@ -13,9 +9,6 @@ public class ApplicationConfiguration {
 
     @Bean
     public S3Client s3Client() {
-        AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
-            AwsBasicCredentials.create("AKIARHUCCYF37ZMSRMVY", "G/V7ju4MinT4h2LiFK/YsTvaBUIypYlnyrDFbt9F")
-        );
-        return S3Client.builder().credentialsProvider(credentialsProvider).region(Region.of("ap-south-1")).build();
+        return S3Client.builder().build();
     }
 }
