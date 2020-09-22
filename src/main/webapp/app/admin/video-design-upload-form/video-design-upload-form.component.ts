f165b89c835dd08c9ad3bae43e50808097f48496
Bala <>
2020-09-23 00:12:40 +0530

    ui-backend fil integration

--- src/main/webapp/app/admin/video-design-upload-form/video-design-upload-form.component.ts
+++ src/main/webapp/app/admin/video-design-upload-form/video-design-upload-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FileUploadService } from './video-design-upload-form.service';
 @Component({
   selector: 'jhi-video-design-upload-form',
   templateUrl: './video-design-upload-form.component.html',
@@ -7,13 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoDesignUploadFormComponent implements OnInit {
   slides: Number[] = [0];
-  constructor() {}
-  
+  constructor(private fileUploadService: FileUploadService) {}
+
   ngOnInit(): void {}
 
   addSlide(): void {
-   this.slides.push(this.slides.length);
+    this.slides.push(this.slides.length);
+  }
+  uploadVideoFile(e: any): void {
+    if (e && e.target) {
+      const fileToBeUploaded = e.target.files[0];
+      this.fileUploadService.uploadFile(fileToBeUploaded).subscribe();
+    }
   }
 }
-
-
