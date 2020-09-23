d39213312ce3f702b000f830b86e941b528e17ec
Bala <>
2020-09-23 22:11:00 +0530

    Background css changes

--- src/main/webapp/app/admin/admin-upload/admin-upload.component.ts
+++ src/main/webapp/app/admin/admin-upload/admin-upload.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'jhi-admin-upload',
   templateUrl: './admin-upload.component.html',
   styleUrls: ['./admin-upload.component.scss'],
 })
-export class AdminUploadComponent implements OnInit {
-  constructor() {}
+export class AdminUploadComponent implements OnInit, OnDestroy {
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.renderer.addClass(this.document.body, 'admin-upload-active');
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(this.document.body, 'admin-upload-active');
+  }
 }
