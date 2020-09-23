4c0f748240aad27b826253768581847b9cae5612
Bala <>
2020-09-23 16:40:26 +0530

    restructing fixes

--- src/main/webapp/app/admin/admin-upload/admin-upload.module.ts
+++ src/main/webapp/app/admin/admin-upload/admin-upload.module.ts
@@ -3,12 +3,13 @@ import { RouterModule } from '@angular/router';
 import { VisualvidSharedModule } from 'app/shared/shared.module';
 
 import { AdminUploadComponent } from './admin-upload.component';
+import { AdminUploadFormComponent } from './admin-upload-form/admin-upload-form.component';
 import { AdminUploadRoute } from './admin-upload.route';
 
 @NgModule({
   imports: [VisualvidSharedModule, RouterModule.forChild([AdminUploadRoute])],
-  declarations: [AdminUploadComponent],
-  exports: [AdminUploadComponent],
+  declarations: [AdminUploadComponent, AdminUploadFormComponent],
+  exports: [AdminUploadComponent, AdminUploadFormComponent],
 })
 export class AdminUploadModule {
   constructor() {}
