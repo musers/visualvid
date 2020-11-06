c042be476b1534a58d07bee72d07e5a81751aa2b
Bala <balagangadhar85@gmail.com>
2020-11-06 11:22:02 +0530

    customer upload changes

--- src/main/webapp/app/admin/admin-upload/admin-media.service.ts
+++ src/main/webapp/app/admin/admin-upload/admin-media.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { AdminMedia } from './admin-upload-form/adminmedia.model';
 import { SERVER_API_URL } from 'app/app.constants';
 import { AdminCategory } from 'app/admin/admin-upload/admin-upload-form/admincategory.model';
+import { AdminMedia } from 'app/admin/admin-upload/admin-upload-form/adminmedia.model';
 
 @Injectable({ providedIn: 'root' })
 export class AdminMediaService {
@@ -12,8 +13,11 @@ export class AdminMediaService {
   public categoryUrl = SERVER_API_URL + '/api/categories/';
   constructor(protected httpClient: HttpClient) {}
 
-  public save(adminMedia: AdminMedia): Observable<any> {
+  public save(adminMedia: AdminMedia): Observable<AdminMedia> {
     return this.httpClient.post(this.resourceUrl, adminMedia);
+  }
+  public get(id: String): Observable<AdminMedia> {
+    return this.httpClient.get(this.resourceUrl+'/'+id);
   }
 
   public getCategories(): Observable<AdminCategory[]> {
--- src/main/webapp/app/user/user-upload/user-upload-form/user-upload-form.component.ts
+++ src/main/webapp/app/user/user-upload/user-upload-form/user-upload-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, Inject, OnInit, OnDestroy, Renderer2 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
+import { AdminMediaService } from 'app/admin/admin-upload/admin-media.service';
+import { AdminMedia } from 'app/admin/admin-upload/admin-upload-form/adminmedia.model';
+
 @Component({
   selector: 'jhi-user-upload-form',
   templateUrl: './user-upload-form.component.html',
   styleUrls: ['./user-upload-form.component.scss'],
 })
 export class UserUploadFormComponent implements OnInit, OnDestroy {
-  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) {}
+  adminMedia: AdminMedia = {};
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document, private renderer: Renderer2,
+    private adminMediaService: AdminMediaService) {}
   ngOnInit(): void {
     this.renderer.addClass(this.document.body, 'customer-upload-active');
+    this.adminMediaService.get('41b62496-720b-47d4-988e-5f318119110e').subscribe((res: AdminMedia) => {
+      if (res != null) {
+        console.log(res);
+        this.adminMedia = res;
+      }
+    });
   }
 
   ngOnDestroy(): void {
