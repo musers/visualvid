2b4e86fcb39767d5af5eab27c0a4166539d1e29b
bsrikanth427 <bsrikanth427@gmail.com>
2020-11-16 22:32:40 +0530

    Fixing Lazy load for OneToMany by adding @Transactional annotations

--- src/main/java/com/ae/visuavid/service/TemplateService.java
+++ src/main/java/com/ae/visuavid/service/TemplateService.java
@@ -12,9 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 @Service
+@Transactional
 public class TemplateService {
     private final Logger log = LoggerFactory.getLogger(TemplateService.class);
 
