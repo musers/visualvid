15068430c8d844215a849394a826c4ffc52b450d
Bala <balagangadhar85@gmail.com>
2020-12-14 20:09:13 +0530

    Removed unused code

--- src/main/webapp/app/admin/dashboard/main/orders/admin-order.model.ts
+++
@@ -1,9 +0,0 @@
-export interface AdminOrderModel {
-  id?: string;
-  name?: string;
-  created?: string;
-  modified?: string;
-  views?: Number;
-  sales?: Number;
-  earnings?: Number;
-}
--- src/main/webapp/app/admin/dashboard/main/video-designs/admin-video.model.ts
+++
@@ -1,9 +0,0 @@
-export interface AdminVideoModel {
-  id?: string;
-  name?: string;
-  created?: string;
-  modified?: string;
-  views?: Number;
-  sales?: Number;
-  earnings?: Number;
-}
