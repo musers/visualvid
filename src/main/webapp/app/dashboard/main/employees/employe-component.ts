1410e227eefc46d241490bfdddc61bf3a453a5c4
Bala <balagangadhar85@gmail.com>
2020-12-30 18:54:13 +0530

    Removed unused variables

--- src/main/webapp/app/dashboard/main/employees/employe-component.ts
+++ src/main/webapp/app/dashboard/main/employees/employe-component.ts
@@ -22,7 +22,7 @@ export class DashboardEmployeComponent implements OnInit {
   }
 
   addNew(): void {
-    const dialogRef = this.dialog.open(DashboardAddEmployeeComponent, {
+    this.dialog.open(DashboardAddEmployeeComponent, {
       width: '20rem',
       height: '30rem',
       data: {
