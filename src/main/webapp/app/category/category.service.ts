6ad4b01d284d0c0eb65e0522cc9362615f5f688d
Bala <balagangadhar85@gmail.com>
2021-01-28 15:37:08 +0530

    Corrected url formats

--- src/main/webapp/app/category/category.service.ts
+++ src/main/webapp/app/category/category.service.ts
@@ -7,32 +7,32 @@ import { SERVER_API_URL } from 'app/app.constants';
 
 @Injectable({ providedIn: 'root' })
 export class CategoryService {
- public resourceUrl = SERVER_API_URL + '/api/category/';
+ public resourceUrl = SERVER_API_URL + '/api/category';
   constructor(private httpClient: HttpClient) { }
 
   public getCategories(): Observable<any> {
     return this.httpClient.get<any>(this.resourceUrl+'/list');
   }
   public createCategory(name: string): Observable<any>{
-    return this.httpClient.post(this.resourceUrl, { name});
+    return this.httpClient.post(this.resourceUrl+'/', { name});
   }
   public createSubCategory(categoryId: string,name: string): Observable<any>{
     return this.httpClient.post(this.resourceUrl+'/'+categoryId+'/subcategory', { name,categoryId});
   }
   getCategoryTree() : Observable<any> {
-   return this.httpClient.get(this.resourceUrl+'tree');
+   return this.httpClient.get(this.resourceUrl+'/tree');
   }
   getAllSubCategories(): Observable<any>{
-    return this.httpClient.get(this.resourceUrl+'subcategory/list');
+    return this.httpClient.get(this.resourceUrl+'/subcategory/list');
   }
   public rename(id: string, type: string, name: string): Observable<any>{
-    return this.httpClient.get(this.resourceUrl+'rename/'+type+'/'+id+'/'+name);
+    return this.httpClient.get(this.resourceUrl+'/rename/'+type+'/'+id+'/'+name);
   }
   public deleteCategory(categoryId: string): Observable<any>{
     return this.httpClient.delete<any>(this.resourceUrl+'/'+categoryId);
   }
   public deleteSubCategory(subCategoryId: string): Observable<any>{
-    return this.httpClient.delete<any>(this.resourceUrl+'subcategory/'+subCategoryId);
+    return this.httpClient.delete<any>(this.resourceUrl+'/subcategory/'+subCategoryId);
   }
 
 }
