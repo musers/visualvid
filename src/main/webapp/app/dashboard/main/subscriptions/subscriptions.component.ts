df68f1e3aa3be66fdeb73403a730d5501a13b561
sai_thota <thotasai2011@gmail.com>
2021-03-11 13:36:29 +0530

    usersubscriptions ui tables

--- src/main/webapp/app/dashboard/main/subscriptions/subscriptions.component.ts
+++ src/main/webapp/app/dashboard/main/subscriptions/subscriptions.component.ts
@@ -6,7 +6,12 @@ import { SubscriptionAddModel } from './add-subscription/add-subscription.model'
 import { SubscriptionService } from './subscriptions.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DashboardAddSubscriptionComponent } from './add-subscription/add-subscription.component';
-import { ColumnSettingsModel, TablePaginationSettingsModel,ITableChangeCallback, TableDataModel} from 'app/shared/table/table-settings.model';
+import {
+  ColumnSettingsModel,
+  TablePaginationSettingsModel,
+  ITableChangeCallback,
+  TableDataModel,
+} from 'app/shared/table/table-settings.model';
 import { TableComponent } from 'app/shared/table/table.component';
 import { OverviewService } from 'app/dashboard/overview/overview.service';
 
@@ -30,13 +35,25 @@ export class DashboardSubscriptionComponent implements OnInit, ITableChangeCallb
 
   @ViewChild('subscriptionsTemplate', { static: true })
   subscriptionsTemplate?: TemplateRef<any>;
-  @ViewChild('subscriptionsOverviewTemplate', {static: true})
+
+  @ViewChild('subscriptionsOverviewTemplate', { static: true })
   subscriptionsOverviewTemplate?: TemplateRef<any>;
 
-  @ViewChild('subscriptionTable', {static: true })
-  subscriptionTable : TableComponent;
+  @ViewChild('userSubscriptionsOverviewTemplate', { static: true })
+  userSubscriptionsOverviewTemplate?: TemplateRef<any>;
+
+  @ViewChild('userSubscriptionsTemplate', { static: true })
+  userSubscriptionsTemplate?: TemplateRef<any>;
+
+  @ViewChild('subscriptionTable', { static: true })
+  subscriptionTable: TableComponent;
+
+  @ViewChild('userSubscriptionsTable', { static: true })
+  userSubscriptionsTable: TableComponent;
 
   columnDefinition: ColumnSettingsModel[] = [];
+  userSubscriptionsColumnDefinition: ColumnSettingsModel[] = [];
+
   tablePaginationSettings: TablePaginationSettingsModel = {
     enablePagination: true,
     pageSize: 1,
@@ -52,10 +69,7 @@ export class DashboardSubscriptionComponent implements OnInit, ITableChangeCallb
   ];
   currentAction = 'action1';
 
-  constructor(
-    public dialog: MatDialog, private subscriptionService: SubscriptionService,
-    public overviewService: OverviewService
-    ) {}
+  constructor(public dialog: MatDialog, private subscriptionService: SubscriptionService, public overviewService: OverviewService) {}
 
   ngOnInit(): void {
     this.getSubscriptionModels();
@@ -90,13 +104,50 @@ export class DashboardSubscriptionComponent implements OnInit, ITableChangeCallb
         cellTemplate: this.subscriptionsTemplate,
       },
     ];
+
+    this.userSubscriptionsColumnDefinition = [
+      {
+        name: 'userId',
+        displayName: 'Subscriber Id#',
+      },
+      {
+        name: 'planName',
+        displayName: 'Plan',
+      },
+      {
+        name: 'userName',
+        displayName: 'Name',
+      },
+      {
+        name: 'startDate',
+        displayName: 'Start Date',
+      },
+      {
+        name: 'endDate',
+        displayName: 'End Date',
+      },
+      {
+        name: 'ordersLeft',
+        displayName: 'Downloads Left(month)',
+      },
+      {
+        name: 'status',
+        displayName: 'Status',
+      },
+      /* {
+            name: 'action',
+            displayName: 'Action',
+            cellTemplate: this.subscriptionsTemplate,
+          }, */
+    ];
   }
 
   getUserSubscriptions(status: String) {
     this.showSubscriptionModels = false;
     this.status = status;
     this.subscriptions = [];
-    this.userSubscriptions = [
+    console.log('user subscriptions:: ');
+    /* this.userSubscriptions = [
       {
         userId: '0001',
         planName: 'Basic',
@@ -127,13 +178,13 @@ export class DashboardSubscriptionComponent implements OnInit, ITableChangeCallb
         status: this.status,
         action: '',
       },
-    ];
+    ];*/
   }
 
   getSubscriptionModels() {
-//     this.subscriptionService.getAllSubscriptionPlans().subscribe(res => {
-//       this.subscriptions = res;
-//     });
+    //     this.subscriptionService.getAllSubscriptionPlans().subscribe(res => {
+    //       this.subscriptions = res;
+    //     });
     this.showSubscriptionModels = true;
     this.status = '';
   }
@@ -163,16 +214,16 @@ export class DashboardSubscriptionComponent implements OnInit, ITableChangeCallb
   }
 
   onNotifySelected(selectedRows: object[]): void {
-    if(selectedRows && selectedRows.length >0){
+    if (selectedRows && selectedRows.length > 0) {
       this.overviewService.updateOverviewTemplate({
-        template : this.subscriptionsOverviewTemplate,
-        data : selectedRows[0]
+        template: this.subscriptionsOverviewTemplate,
+        data: selectedRows[0],
       });
     }
   }
 
   onDoubleClick(data: any): void {
-//     window.location.href = '/customer/upload/';
+    //     window.location.href = '/customer/upload/';
   }
 
   search(evt: any): void {
@@ -182,14 +233,35 @@ export class DashboardSubscriptionComponent implements OnInit, ITableChangeCallb
     console.log('action', action);
     console.log('element:', element);
   }
-  getData(tableDataModel: TableDataModel): void {
-       this.subscriptionService.getAllSubscriptionPlans(tableDataModel).subscribe(resp =>{
-          this.subscriptions = resp;
-          const data = {
-            rowData : resp,
-            total : 12
-          }
-          this.subscriptionTable.setData(data);
-       })
+
+  onUserSubscriptionsNotifySelected(selectedRows: object[]): void {
+    if (selectedRows && selectedRows.length > 0) {
+      this.overviewService.updateOverviewTemplate({
+        template: this.userSubscriptionsOverviewTemplate,
+        data: selectedRows[0],
+      });
     }
+  }
+
+  getData(tableDataModel: TableDataModel): void {
+    if (this.showSubscriptionModels) {
+      this.subscriptionService.getAllSubscriptionPlans(tableDataModel).subscribe(resp => {
+        this.subscriptions = resp;
+        const data = {
+          rowData: resp,
+          total: 12,
+        };
+        this.subscriptionTable.setData(data);
+      });
+    } else {
+      this.subscriptionService.getAllUserSubscriptions(tableDataModel).subscribe(resp => {
+        this.userSubscriptions = resp;
+        const data = {
+          rowData: resp,
+          total: 12,
+        };
+        this.userSubscriptionsTable.setData(data);
+      });
+    }
+  }
 }
--- src/main/webapp/app/dashboard/main/subscriptions/subscriptions.service.ts
+++ src/main/webapp/app/dashboard/main/subscriptions/subscriptions.service.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 import { SERVER_API_URL } from '../../../app.constants';
 import { SubscriptionAddModel } from './add-subscription/add-subscription.model';
 import { TableDataModel } from 'app/shared/table/table-settings.model';
-import { delay } from "rxjs/operators";
+import { delay } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class SubscriptionService {
@@ -26,7 +26,7 @@ export class SubscriptionService {
   }
 
   public getAllSubscriptionPlans(tableDataModel: TableDataModel): Observable<any> {
-    console.log('tableDataModel',tableDataModel);
+    console.log('tableDataModel', tableDataModel);
     const data = [
       {
         id: Math.random().toString(),
@@ -45,9 +45,48 @@ export class SubscriptionService {
         revisions: 'Revision 2',
         status: 'Active',
         action: 'inactive',
-      }
+      },
     ];
     console.log('subscriptions data: ', data);
+    return of(data).pipe(delay(3000));
+  }
+
+  public getAllUserSubscriptions(tableDataModel: TableDataModel): Observable<any> {
+    console.log('tableDataModel', tableDataModel);
+    const data = [
+      {
+        userId: '0001',
+        planName: 'Basic',
+        userName: 'Anil Kumar',
+        startDate: '19 Nov,2020',
+        endDate: '19 Nov,2020',
+        ordersLeft: '5/10',
+        // status: this.status,
+        action: '',
+      },
+      {
+        userId: '0002',
+        planName: 'Basic',
+        userName: 'Anil Kumar',
+        startDate: '19 Nov,2020',
+        endDate: '19 Nov,2020',
+        ordersLeft: '5/10',
+        // status: this.status,
+        action: '',
+      },
+      {
+        userId: '0003',
+        planName: 'Basic',
+        userName: 'Anil Kumar',
+        startDate: '19 Nov,2020',
+        endDate: '19 Nov,2020',
+        ordersLeft: '5/10',
+        //  status: this.status,
+        action: '',
+      },
+    ];
+
+    console.log('User Subscriptions Data: ', data);
     return of(data).pipe(delay(3000));
   }
 }
