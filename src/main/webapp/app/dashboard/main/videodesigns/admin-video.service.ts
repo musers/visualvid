4212ad3781e93e55e69e7070986a8ea77d67a586
Bala <balagangadhar85@gmail.com>
2021-03-17 22:25:33 +0530

    Jhi table integration

--- src/main/webapp/app/dashboard/main/videodesigns/admin-video.service.ts
+++ src/main/webapp/app/dashboard/main/videodesigns/admin-video.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { createRequestOption, Pagination } from 'app/shared/util/request-util';
 
+import { TableDataModel } from 'app/shared/table/table-settings.model';
+import { delay } from 'rxjs/operators';
 import { SERVER_API_URL } from 'app/app.constants';
 import { AdminVideoModel } from './admin-video.model';
 
@@ -20,4 +22,8 @@ export class AdminVideoService {
     const options = createRequestOption(req);
     return this.httpClient.get<AdminVideoModel[]>(this.resourceUrl + '/page', { params: options, observe: 'response' });
   }
+  public getAll(tableDataModel: TableDataModel): Observable<any> {
+    return this.httpClient.get<AdminVideoModel[]>(this.resourceUrl + '/page');
+  }
 }
+
--- src/main/webapp/app/dashboard/main/videodesigns/videodesigns.component.ts
+++ src/main/webapp/app/dashboard/main/videodesigns/videodesigns.component.ts
@@ -1,27 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component,Input, OnInit, AfterViewInit, ViewChild, TemplateRef, SimpleChanges, OnChanges } from '@angular/core';
 import { AdminVideoService } from './admin-video.service';
 import { AdminVideoModel } from './admin-video.model';
-import { ColumnSettingsModel, TablePaginationSettingsModel } from 'app/shared/table/table-settings.model';
-// import { Pagination } from '../../../../../app/shared/util/request-util';
-// import { HttpResponse } from '@angular/common/http';
+import { OverviewService } from 'app/dashboard/overview/overview.service';
+import { ColumnSettingsModel, ITableChangeCallback, TableDataModel } from 'app/shared/table/table-settings.model';
+import { TableComponent } from 'app/shared/table/table.component';
 
 @Component({
   selector: 'jhi-dashboard-videodesigns',
   templateUrl: './videodesigns.component.html',
   styleUrls: ['videodesigns.component.scss'],
 })
-export class DashboardVideoDesignsComponent implements OnInit {
+export class DashboardVideoDesignsComponent implements OnInit, ITableChangeCallback, AfterViewInit, OnChanges  {
   @Input() videoDesigns?: AdminVideoModel[];
+
+  @ViewChild('table', { static: false })
+  table: TableComponent;
+  @ViewChild('overviewTemplate', { static: false })
+  overviewTemplate?: TemplateRef<any>;
+
   count: Number = 0;
   columnDefinition: ColumnSettingsModel[] = [];
-  tablePaginationSettings: TablePaginationSettingsModel = {
-    enablePagination: true,
-    pageSize: 10,
-    pageSizeOptions: [10, 20, 30],
-    showFirstLastButtons: true,
-  };
-  rowData: Array<AdminVideoModel> = [];
-  constructor(protected adminVideoService: AdminVideoService) {
+  constructor(
+    protected adminVideoService: AdminVideoService,
+    protected overviewService: OverviewService) {
     this.columnDefinition = [
       {
         name: 'name',
@@ -42,39 +43,56 @@ export class DashboardVideoDesignsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    //    this.adminVideoService.findAllByPage({
-    //        page: 0,
-    //        size: 10,
-    //        sort: [],
-    //       })
-    //        .subscribe((res: HttpResponse<AdminVideoModel[]>) => {
-    //                 if(res != null && res.body != null) {
-    //                     this.videoDesigns = res.body;
-    //                 }
-    //        });
-
-    this.adminVideoService.findAll().subscribe((res: AdminVideoModel[]) => {
-      if (res != null) {
-        // this.videoDesigns = res;
-        this.rowData = res;
-        this.rowData.forEach(vd => {
-          vd.created = 'Sep23, 2020';
-          vd.modified = 'Sep23, 2020';
-          vd.views = 500;
-          vd.sales = 250;
-          vd.earnings = 75000;
-        });
-      }
-    });
+//     this.adminVideoService.findAll().subscribe((res: AdminVideoModel[]) => {
+//       if (res != null) {
+//         // this.videoDesigns = res;
+//         this.rowData = res;
+//         this.rowData.forEach(vd => {
+//           vd.created = 'Sep23, 2020';
+//           vd.modified = 'Sep23, 2020';
+//           vd.views = 500;
+//           vd.sales = 250;
+//           vd.earnings = 75000;
+//         });
+//       }
+//     });
   }
   editVideoDesign(vd: AdminVideoModel): void {
     window.location.href = '/admin/upload/' + vd.id;
   }
-  onNotifySelected(selectedRows: object[]): void {
-    console.log(selectedRows);
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+    this.table.requestData();
   }
+
+  onRowSelect(selectedRows: object[]): void {
+    if (selectedRows && selectedRows.length > 0) {
+      this.overviewService.updateOverviewTemplate({
+        template: this.overviewTemplate,
+        data: selectedRows[0],
+      });
+    }
+  }
+
   search(evt: any): void {
-    console.log(evt);
-    // TODO call a back-end service awith evt.query and map result to this.rowData;
+    if(this.table){
+      this.table.search(evt.query);
+    }
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnchanges');
+    if (changes.searchText) {
+      this.search(changes.searchText.currentValue);
+    }
+  }
+
+  getData(tableDataModel: TableDataModel): void {
+    this.adminVideoService.getAll(tableDataModel).subscribe(resp => {
+      const data = {
+        rowData: resp.content,
+        total: 12,
+      };
+      this.table.setData(data);
+    });
   }
 }
