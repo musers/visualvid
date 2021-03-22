ce8592ca04d713358f43df20c325575ca271f3e2
sai_thota <thotasai2011@gmail.com>
2021-03-22 14:20:55 +0530

    jhi table integration for Orders

--- src/main/webapp/app/dashboard/main/orders/orders.component.ts
+++ src/main/webapp/app/dashboard/main/orders/orders.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit,AfterViewInit,TemplateRef, ViewChild} from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, TemplateRef, ViewChild, SimpleChanges, OnChanges } from '@angular/core';
 import { OrderModel } from 'app/order/order.model';
 import { OrderService } from 'app/order/order.service';
-import { ColumnSettingsModel, TablePaginationSettingsModel } from 'app/shared/table/table-settings.model';
+import { OverviewService } from 'app/dashboard/overview/overview.service';
+import { ColumnSettingsModel, ITableChangeCallback, TableDataModel } from 'app/shared/table/table-settings.model';
+import { TableComponent } from 'app/shared/table/table.component';
 
 export interface Action {
   value: string;
@@ -13,59 +15,105 @@ export interface Action {
   templateUrl: './orders.component.html',
   styleUrls: ['orders.scss'],
 })
-
-export class DashboardOrdersComponent implements OnInit,AfterViewInit  {
+export class DashboardOrdersComponent implements OnInit, AfterViewInit, ITableChangeCallback, OnChanges {
   @Input() orders?: OrderModel[];
-  @ViewChild('orderStatusTemplate',{static: true}) orderStatusTemplate?: TemplateRef<any>;
+  @ViewChild('orderStatusTemplate', { static: true }) orderStatusTemplate?: TemplateRef<any>;
+
+  @ViewChild('table', { static: false })
+  table: TableComponent;
+
+  @ViewChild('overviewTemplate', { static: false })
+  overviewTemplate?: TemplateRef<any>;
+
+  @ViewChild('actionTemplate', { static: true })
+  actionTemplate?: TemplateRef<any>;
+
   columnDefinition: ColumnSettingsModel[] = [];
-  tablePaginationSettings: TablePaginationSettingsModel = {
-    enablePagination: true,
-    pageSize: 10,
-    pageSizeOptions: [10, 20, 30],
-    showFirstLastButtons: true,
-  };
+
   rowData: Array<OrderModel> = [];
   actions: Action[] = [
-    {viewValue: 'Action 1', value: 'action1'},
-    {viewValue: 'Action 2', value: 'action2'},
-    {viewValue: 'Action 3', value: 'action3'}
+    { viewValue: 'Action 1', value: 'action1' },
+    { viewValue: 'Action 2', value: 'action2' },
+    { viewValue: 'Action 3', value: 'action3' },
   ];
   currentAction = 'action1';
-  constructor(private orderService: OrderService) {
+
+  constructor(private orderService: OrderService, private overviewService: OverviewService) {}
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+    this.table.requestData();
   }
 
-  ngAfterViewInit(): void{
-    console.log('this.orderStatusTemplate',this.orderStatusTemplate);
-  }
   ngOnInit(): void {
-      this.columnDefinition = [
-            {
-              name: 'name',
-              displayName: 'Name'
-            },
-            {
-              name: 'orderId',
-              displayName: 'Order ID #'
-            },
-            {
-              name: 'orderStatus',
-              displayName: 'Status',
-              cellTemplate: this.orderStatusTemplate
-            },
-          ];
+    this.columnDefinition = [
+      {
+        name: 'salesId',
+        displayName: 'Sales Id #',
+      },
+      {
+        name: 'orderId',
+        displayName: 'Order ID #',
+      },
+      {
+        name: 'name',
+        displayName: 'Name',
+      },
+      {
+        name: 'assignTo',
+        displayName: 'Assign to',
+      },
+      {
+        name: 'orderStatus',
+        displayName: 'Status',
+        cellTemplate: this.orderStatusTemplate,
+      },
+      {
+        name: 'timeLeft',
+        displayName: 'Time Left',
+      },
+      {
+        name: 'action',
+        displayName: 'Action #',
+        cellTemplate: this.actionTemplate,
+      },
+    ];
     this.orderService.findAll().subscribe(data => {
       console.log(data);
       this.rowData = data;
     });
   }
-  onNotifySelected(selectedRows: object[]): void {
-    console.log(selectedRows);
+
+  onRowSelect(selectedRows: object[]): void {
+    this.overviewService.updateOverviewTemplate(this.overviewTemplate, selectedRows);
   }
-  onDoubleClick(data: any): void {
-    window.location.href = '/customer/upload/' + data.adminMediaId + '/' + data.id;
-  }
+
   search(evt: any): void {
-    console.log(evt);
-    // TODO call a back-end service awith evt.query and map result to this.rowData;
+    if (this.table) {
+      this.table.search(evt.query);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnchanges');
+    if (changes.searchText) {
+      this.search(changes.searchText.currentValue);
+    }
+  }
+
+  getData(tableDataModel: TableDataModel): void {
+    // this.orderService.getAll(tableDataModel).subscribe(resp => {
+    this.orderService.findAll().subscribe(resp => {
+      const data = {
+        rowData: resp,
+        total: 12,
+      };
+      this.table.setData(data);
+    });
+  }
+
+  onActionSelect(action: string, element: any): void {
+    console.log('action', action);
+    console.log('element:', element);
   }
 }
